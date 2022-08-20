547047e918197f9942998506e10247500cd963d1
Siddhant Prateek <siddhantprateek@gmail.com>
2022-08-20 21:04:10 +0530

    home page created

--- client/src/pages/home/home.pages.jsx
+++ client/src/pages/home/home.pages.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 // components
+import { Hero, Abstract } from '../../components'
 
 
 // styles
@@ -8,8 +9,18 @@ import './home.styles.css'
 
 const Home = () => {
   return (
-    <div className='home'>
-        <h1>Home</h1>
+    <div className='home-page'>
+      <section>
+        <div className="spreads"></div>
+        <div className="spreads"></div>
+      </section>
+      <div className="page-content">
+        <Hero/>
+      </div>
+
+      <div className="section-meditate">
+        <Abstract/>
+      </div>
     </div>
   )
 }
