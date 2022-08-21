931afbd164a24e84eb82832de5988e4de14ddd88
Siddhant Prateek <siddhantprateek@gmail.com>
2022-08-21 06:48:05 +0530

    home page finished

--- client/src/pages/home/home.pages.jsx
+++ client/src/pages/home/home.pages.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 // components
-import { Hero, Abstract } from '../../components'
+import { Hero, Abstract, FeaturedTherapy, HealthMatter } from '../../components'
 
 
 // styles
@@ -16,10 +16,21 @@ const Home = () => {
       </section>
       <div className="page-content">
         <Hero/>
-      </div>
 
-      <div className="section-meditate">
-        <Abstract/>
+        <div className="section-meditate">
+          <Abstract/>
+          <div className="quote-section">
+             <p>You <b>don&lsquo;t</b> have to <b>struggle</b> in silence!</p>
+            <p>Life is <b>tough</b> but so are <b>you</b>.</p>
+          </div>
+        </div>
+
+        <div className="therapy-sec">
+          <FeaturedTherapy/>
+        </div>
+        <div className="health-section">
+          <HealthMatter />
+        </div>
       </div>
     </div>
   )
