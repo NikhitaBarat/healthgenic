5d8ea149dc69d051801faddbea9af64ee6f71dae
S-170802 <shahanashamim002@gmail.com>
2022-08-21 09:53:46 +0530

    third commit

--- client/src/pages/team/team.pages.jsx
+++ client/src/pages/team/team.pages.jsx
@@ -14,11 +14,25 @@ const Teams = () => {
         <div className="image">
         <img src={shapes} alt=""/>
         </div>
-        <h1>Our Team</h1>
+        <h1 className="heading">Our Team</h1>
         <div className="member">
-        <img src={shahana} alt=""/> 
-        <img src={nikhita} alt=""/>
-        <img src={siddhant} alt=""/>
+        
+        <div className="member1"> 
+        <img className="profileimg" src={shahana} alt=""/> 
+        <div class="caption">Shahana</div>
+        </div>
+       
+        <div className="member2"> 
+        <img  className="profileimg" src={nikhita} alt=""/>
+        <div class="caption">Nikhita</div>
+        </div>
+
+        <div className="member3">
+        <img  className="profileimg" src={siddhant} alt=""/>
+        <div class="caption">Siddhant</div>
+
+        </div>
+
         </div>
         
     </div>
